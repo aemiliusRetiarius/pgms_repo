0a59ef65ee913dcf6e6724b9293df2f292f1fde8
Emile Visser <emile.visser@gmail.com>
2021-08-16 13:24:51 +0200

    Added rest of assignment

--- week1/factorops.py
+++ week1/factorops.py
@@ -13,13 +13,14 @@ print(cpd_z)
 
 ###################
 cpd_joint = (cpd_z.product(cpd_y, inplace=False)).product(cpd_x,inplace=False)
+cpd_joint = cpd_joint.to_factor()
 
 print("Joint Distribution:")
 print(cpd_joint)
 
 ###################
 cpd_joint_mar = cpd_joint.marginalize(['Y'], inplace=False)
-#cpd_joint_mar = cpd_joint.marginal_distribution(['X','Z'], inplace=False)
+
 
 print("Marginalized Joint Distribution:")
 print(cpd_joint_mar)
@@ -27,7 +28,22 @@ print(cpd_joint_mar)
 ##################
 
 cpd_joint_mar_x = cpd_joint_mar.reduce([('X', 1)], inplace=False)
+cpd_joint_mar_x.normalize()
 
 print("Marginalized Joint Distribution with X=1:")
 print(cpd_joint_mar_x)
 
+##################
+
+cpd_joint_mar_z = cpd_joint_mar.reduce([('Z', 2)], inplace=False)
+cpd_joint_mar_z.normalize()
+
+print("Marginalized Joint Distribution with Z=2:")
+print(cpd_joint_mar_z)
+
+##################
+
+cpd_joint_mar.marginalize(['X'])
+
+print("Marginalized distribution for Z:")
+print(cpd_joint_mar)
