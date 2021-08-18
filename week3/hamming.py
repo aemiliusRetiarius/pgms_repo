45bf1c7c71448144a6dd26641e8a5fa4db891a15
Emile Visser <emile.visser@gmail.com>
2021-08-18 13:57:25 +0200

    Added joint

--- week3/hamming.py
+++ week3/hamming.py
@@ -5,4 +5,7 @@ disc_bit_4 = DiscreteFactor(['b0', 'b1', 'b2', 'b4'], [2, 2, 2, 2], [1, 0, 0, 1,
 disc_bit_5 = DiscreteFactor(['b0', 'b2', 'b3', 'b5'], [2, 2, 2, 2], [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1])
 disc_bit_6 = DiscreteFactor(['b0', 'b1', 'b3', 'b6'], [2, 2, 2, 2], [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1])
 
-print(disc_bit_4)
+disc_joint = (disc_bit_4.product(disc_bit_5, inplace=False)).product(disc_bit_6, inplace=False)
+disc_joint.normalize()
+
+print(disc_joint)
