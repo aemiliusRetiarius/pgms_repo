41f1d024c49da16e63ef0bef51d85fd20c702841
Emile Visser <emile.visser@gmail.com>
2021-08-28 18:53:58 +0200

    Added factor definitions

--- week4/cluster_hamming/src/cluster_hamming.cc
+++ week4/cluster_hamming/src/cluster_hamming.cc
@@ -13,75 +13,78 @@
 using namespace std;
 using namespace emdw;
 
-
 /**                            **/
 
 #include "emdw.hpp"
 
 int main(int, char *argv[]) {
 
-  // Type Setup
-    typedef unsigned T;
-    typedef DiscreteTable<T> DT;
-    vector<rcptr<Factor>> factors;
-    rcptr<vector<T>> bit_domain (new vector<T>{0,1});
-    enum{B0,B1,B2,B3,B4,B5,B6,R0,R1,R2,R3,R4,R5,R6};
+ // Type Setup
+  typedef unsigned T;
+  typedef DiscreteTable<T> DT;
+  vector<rcptr<Factor>> factors;
+  rcptr<vector<T>> bit_domain (new vector<T>{0,1});
+  enum{B0,B1,B2,B3,B4,B5,B6,R0,R1,R2,R3,R4,R5,R6};
 
-  factors.push_back(
-    uniqptr<DT>(
-      new DT({B0, B1, B2, B4},{bit_domain, bit_domain, bit_domain, bit_domain}, 0.0,
-        {
-          {{0,0,0,0},1.0},
-          {{0,0,1,1},1.0},
-          {{0,1,0,1},1.0},
-          {{0,1,1,0},1.0},
-          {{1,0,0,1},1.0},
-          {{1,0,1,0},1.0},
-          {{1,1,0,0},1.0},
-          {{1,1,1,1},1.0},
-        }
-      ) 
-    ) 
-  );
+  //P(B4|B0,B1,B2)
+    rcptr<Factor> C7(
+        uniqptr<DT>(
+            new DT({B0,B1,B2,B4}, {bit_domain,bit_domain,bit_domain,bit_domain}, 0.0,
+                {
+                    {{0,0,0,0},1.0},
+                    {{0,0,1,1},1.0},
+                    {{0,1,0,1},1.0},
+                    {{0,1,1,0},1.0},
+                    {{1,0,0,1},1.0},
+                    {{1,0,1,0},1.0},
+                    {{1,1,0,0},1.0},
+                    {{1,1,1,1},1.0},
+                }
+            )
+        )
+    );
 
-  factors.push_back(
-    uniqptr<DT>(
-      new DT({B0, B2, B3, B5},{bit_domain, bit_domain, bit_domain, bit_domain}, 0.0,
-        {
-          {{0,0,0,0},1.0},
-          {{0,0,1,1},1.0},
-          {{0,1,0,1},1.0},
-          {{0,1,1,0},1.0},
-          {{1,0,0,1},1.0},
-          {{1,0,1,0},1.0},
-          {{1,1,0,0},1.0},
-          {{1,1,1,1},1.0},
-        }
-      ) 
-    ) 
-  );
+    //P(B5|B0,B2,B3)
+    rcptr<Factor> C8(
+        uniqptr<DT>(
+            new DT({B0,B2,B3,B5}, {bit_domain,bit_domain,bit_domain,bit_domain}, 0.0,
+                {
+                    {{0,0,0,0},1.0},
+                    {{0,0,1,1},1.0},
+                    {{0,1,0,1},1.0},
+                    {{0,1,1,0},1.0},
+                    {{1,0,0,1},1.0},
+                    {{1,0,1,0},1.0},
+                    {{1,1,0,0},1.0},
+                    {{1,1,1,1},1.0},
+                }
+            )
+        )
+    );
 
-  factors.push_back(
-    uniqptr<DT>(
-      new DT({B0, B1, B3, B6},{bit_domain, bit_domain, bit_domain, bit_domain}, 0.0,
-        {
-          {{0,0,0,0},1.0},
-          {{0,0,1,1},1.0},
-          {{0,1,0,1},1.0},
-          {{0,1,1,0},1.0},
-          {{1,0,0,1},1.0},
-          {{1,0,1,0},1.0},
-          {{1,1,0,0},1.0},
-          {{1,1,1,1},1.0},
-        }
-      ) 
-    ) 
-  );
+    //P(B6|B0,B1,B3)
+    rcptr<Factor> C9(
+        uniqptr<DT>(
+            new DT({B0,B1,B3,B6}, {bit_domain,bit_domain,bit_domain,bit_domain}, 0.0,
+                {
+                    {{0,0,0,0},1.0},
+                    {{0,0,1,1},1.0},
+                    {{0,1,0,1},1.0},
+                    {{0,1,1,0},1.0},
+                    {{1,0,0,1},1.0},
+                    {{1,0,1,0},1.0},
+                    {{1,1,0,0},1.0},
+                    {{1,1,1,1},1.0},
+                }
+            )
+        )
+    );
 
-  double P_e = 0.01;
+    double P_e = 0.01;
+    //double P_e = 0.9;
 
     //P(R0|B0)
-    factors.push_back(
+    rcptr<Factor> C0(
         uniqptr<DT>(
             new DT({B0,R0}, {bit_domain,bit_domain}, 0.0,
                 {
@@ -95,7 +98,7 @@ int main(int, char *argv[]) {
     );
 
     //P(R1|B1)
-    factors.push_back(
+    rcptr<Factor> C1(
         uniqptr<DT>(
             new DT({B1,R1}, {bit_domain,bit_domain}, 0.0,
                 {
@@ -109,7 +112,7 @@ int main(int, char *argv[]) {
     );
 
     //P(R2|B2)
-    factors.push_back(
+    rcptr<Factor> C2(
         uniqptr<DT>(
             new DT({B2,R2}, {bit_domain,bit_domain}, 0.0,
                 {
@@ -123,7 +126,7 @@ int main(int, char *argv[]) {
     );
 
     //P(R3|B3)
-    factors.push_back(
+    rcptr<Factor> C3(
         uniqptr<DT>(
             new DT({B3,R3}, {bit_domain,bit_domain}, 0.0,
                 {
@@ -137,7 +140,7 @@ int main(int, char *argv[]) {
     );
 
     //P(R4|B4)
-    factors.push_back(
+    rcptr<Factor> C4(
         uniqptr<DT>(
             new DT({B4,R4}, {bit_domain,bit_domain}, 0.0,
                 {
@@ -151,7 +154,7 @@ int main(int, char *argv[]) {
     );
 
     //P(R5|B5)
-    factors.push_back(
+    rcptr<Factor> C5(
         uniqptr<DT>(
             new DT({B5,R5}, {bit_domain,bit_domain}, 0.0,
                 {
@@ -165,7 +168,7 @@ int main(int, char *argv[]) {
     );
 
     //P(R6|B6)
-    factors.push_back(
+    rcptr<Factor> C6(
         uniqptr<DT>(
             new DT({B6,R6}, {bit_domain,bit_domain}, 0.0,
                 {
