80b7922a2774dee1f49692815e3fc88fc0c15208
Emile Visser <emile.visser@gmail.com>
2021-08-30 19:45:12 +0200

    Updated

---
+++ week4/cluster_hamming_loop/build/src/CMakeFiles/cluster_hamming_loop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluster_hamming_loop.dir/cluster_hamming_loop.cc.o"
+  "cluster_hamming_loop"
+  "cluster_hamming_loop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cluster_hamming_loop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
